356f3a5a82ea8ca7a0ff9359cda23ac334e7abc5
pediazr <pedro.neox@gmail.com>
2024-08-21 10:20:34 -0400

    primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    });
+});
+
+function mostrarAlerta() {
+    alert("¡Contacto Registrado Satisfactoriamente!");
+}
